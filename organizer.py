3f0196e56a44b3e797d7f5814f82ebbc58333544
Nate Stutte <ngs33@drexel.edu>
2021-02-17 14:20:10 -0500

    Added error handling for file existing in folder

--- organizer.py
+++ organizer.py
@@ -1,12 +1,17 @@
 from os import listdir, mkdir, environ
 from os.path import isfile, join
-from shutil import move
+import shutil
+import logging
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 def getfiles(dir):
-    return [f for f in listdir(dir) if isfile(join(dir, f)) and not 'organizer.py']
+    return [f for f in listdir(dir) if isfile(join(dir, f)) and f != 'organizer.py']
 
 def getfolders(dir):
     return [f for f in listdir(dir) if not isfile(join(dir, f))]
+
 
 desktop_path = join(join(environ['USERPROFILE']), 'Desktop')
 
@@ -15,15 +20,16 @@ folders = getfolders(desktop_path)
 extensions = []
 
 for a in files:
-    print(a)
     a_ext = a.split('.')[-1].lower()
     if a_ext not in extensions:
         extensions.append(a_ext)
         if a_ext not in folders:
             mkdir(f'{desktop_path}\\{a_ext}')
             folders.append(a_ext)
-    move(f'{desktop_path}\\{a}', f'{desktop_path}\\{a_ext}')
-
-        
-
-           
+            logger.info(f"Folder \"{desktop_path}\\{a_ext}\" created.")
+    try:
+        shutil.move(f'{desktop_path}\\{a}', f'{desktop_path}\\{a_ext}')
+    except shutil.Error as err:
+        logger.error(f"\"{a}\" already exists within folder \"{desktop_path}\\{a_ext}\". File will not be moved.")
+    else:
+        logger.info(f"Moved file \"{a}\" to \"{desktop_path}\\{a_ext}\"")
